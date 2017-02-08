ef4f8acbb88f7b5fb48bd9606487ad66cc4cccb1
JF <jf@whatson-web.com>
2017-02-08 12:11:45 +0100

    translations

--- Controller/Backend/PageController.php
+++ Controller/Backend/PageController.php
@@ -22,253 +22,234 @@ use WH\LibBundle\Utils\Inflector;
 class PageController extends BaseController
 {
 
-	public $bundlePrefix = 'WH';
-	public $bundle = 'CmsBundle';
-	public $entity = 'Page';
+    public $bundlePrefix = 'WH';
+    public $bundle = 'CmsBundle';
+    public $entity = 'Page';
 
-	/**
-	 * @Route("/index/{parentId}", name="bk_wh_cms_page_index", requirements={"parentId": ".*"}, defaults={"parentId": null})
-	 *
-	 * @param         $parentId
-	 * @param Request $request
-	 *
-	 * @return string
-	 */
-	public function indexAction($parentId = null, Request $request)
-	{
-		$arguments = array(
-			'parent.id' => $parentId,
-		);
+    /**
+     * @Route("/index/{parentId}", name="bk_wh_cms_page_index", requirements={"parentId": ".*"}, defaults={"parentId": null})
+     *
+     * @param         $parentId
+     * @param Request $request
+     *
+     * @return string
+     */
+    public function indexAction($parentId = null, Request $request)
+    {
+        $arguments = array(
+            'parent.id' => $parentId,
+        );
 
-		$indexController = $this->get('bk.wh.back.index_controller');
+        $indexController = $this->get('bk.wh.back.index_controller');
 
-		return $indexController->index($this->getEntityPathConfig(), $request, $arguments);
-	}
+        return $indexController->index($this->getEntityPathConfig(), $request, $arguments);
+    }
 
-	/**
-	 * @Route("/create/", name="bk_wh_cms_page_create")
-	 *
-	 * @param Request $request
-	 *
-	 * @return mixed
-	 */
-	public function createAction(Request $request)
-	{
-		$createController = $this->get('bk.wh.back.create_controller');
+    /**
+     * @Route("/create/", name="bk_wh_cms_page_create")
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function createAction(Request $request)
+    {
+        $createController = $this->get('bk.wh.back.create_controller');
 
-		return $createController->create($this->getEntityPathConfig(), $request);
-	}
+        return $createController->create($this->getEntityPathConfig(), $request);
+    }
 
-	/**
-	 * @Route("/update/{id}", name="bk_wh_cms_page_update")
-	 *
-	 * @param         $id
-	 * @param Request $request
-	 *
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function updateAction($id, Request $request)
-	{
-		$this->backendTranslator = $this->container->get('bk.wh.back.translator');
+    /**
+     * @Route("/update/{id}", name="bk_wh_cms_page_update")
+     *
+     * @param         $id
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function updateAction($id, Request $request)
+    {
+        $em = $this->container->get('doctrine')->getManager();
 
-		$entityPathConfig = $this->getEntityPathConfig();
-		$this->setTranslateDomain($entityPathConfig);
+        $entityPathConfig = $this->getEntityPathConfig();
 
-		$em = $this->container->get('doctrine')->getManager();
+        $data = $em->getRepository($this->getRepositoryName($entityPathConfig))->get(
+            'one',
+            array(
+                'conditions' => array(
+                    Inflector::camelize($entityPathConfig['entity']) . '.id' => $id,
+                ),
+            )
+        );
 
-		$page = $em->getRepository($this->getRepositoryName($entityPathConfig))->get(
-			'one',
-			array(
-				'conditions' => array(
-					Inflector::camelize($entityPathConfig['entity']) . '.id' => $id,
-				),
-			)
-		);
-		if (!$page) {
-			return $this->redirect($this->getActionUrl($entityPathConfig, 'index'));
-		}
-		$pageTemplate = '';
-		if ($page->getPageTemplateSlug()) {
-			$pageTemplate = $this->getParameter('wh_cms_templates')[$page->getPageTemplateSlug()];
-		}
-		if (!empty($pageTemplate['backendController'])) {
+        if (!$data) {
+            return $this->redirect($this->getActionUrl($entityPathConfig, 'index'));
+        }
 
-			return $this->forward(
-				$pageTemplate['backendController'] . ':update',
-				array(
-					'id'      => $id,
-					'request' => $request,
-				)
-			);
-		}
+        $pageTemplate = '';
+        if ($data->getPageTemplateSlug()) {
+            $pageTemplate = $this->getParameter('wh_cms_templates')[$data->getPageTemplateSlug()];
+        }
+        if (!empty($pageTemplate['backendController'])) {
 
-		$renderVars = array();
-
-		if (!empty($pageTemplate['updateConfig'])) {
-			$config = $this->getOverrideConfig($pageTemplate['updateConfig']);
-		} else {
-			$config = $this->getConfig($entityPathConfig, 'update');
-		}
-		$globalConfig = $this->getGlobalConfig($entityPathConfig);
-
-		$renderVars['globalConfig'] = $globalConfig;
-
-        $renderVars['title'] = $this->backendTranslator->trans($config['title']);
-
-		$formFields = $this->getFormFields($config['formFields'], $entityPathConfig);
-
-		$form = $this->getEntityForm($formFields, $entityPathConfig, $page);
-
-		$renderVars['breadcrumb'] = $this->getBreadcrumb(
-			$config['breadcrumb'],
-			$entityPathConfig,
-			$page
-		);
-
-		$form->handleRequest($request);
-
-		if ($form->isSubmitted()) {
-
-			$page = $form->getData();
-
-			$em->persist($page);
-			$em->flush();
-
-			$redirectUrl = $this->getActionUrl($entityPathConfig, 'index', $page);
-			if ($form->has('saveAndStay') && $form->get('saveAndStay')->isClicked()) {
-				$redirectUrl = $this->getActionUrl($entityPathConfig, 'update', $page);
-			}
-
-			if ($request->isXmlHttpRequest()) {
-
-				return new JsonResponse(
-					array(
-						'success'  => true,
-						'redirect' => $redirectUrl,
-					)
-				);
-			}
-
-			return $this->redirect($redirectUrl);
-		} else {
-
-			$form->setData($page);
-		}
-
-		$form = $form->createView();
-		$renderVars['form'] = $form;
-		$renderVars['formFields'] = $formFields;
-
-        if (!empty($config['central']['viewLink']['name'])) {
-            $config['central']['viewLink']['name'] = $this->backendTranslator->trans(
-                $config['central']['viewLink']['name']
+            return $this->forward(
+                $pageTemplate['backendController'] . ':update',
+                array(
+                    'id'      => $id,
+                    'request' => $request,
+                )
             );
         }
-		if (!empty($config['central']['viewLink']['action'])) {
-			$config['central']['viewLink']['url'] = $this->getActionUrl(
-				$entityPathConfig,
-				$config['central']['viewLink']['action'],
-				$page
-			);
-		}
+
+        $renderVars = array();
+
+        $config = $this->getConfig($entityPathConfig, 'update');
+        $globalConfig = $this->getGlobalConfig($entityPathConfig);
+
+        $renderVars['globalConfig'] = $globalConfig;
+
+        $renderVars['title'] = $config['title'];
+
+        $formFields = $this->getFormFields($config['formFields'], $entityPathConfig);
+
+        $form = $this->getEntityForm($formFields, $entityPathConfig, $data);
+
+        $renderVars['breadcrumb'] = $this->getBreadcrumb(
+            $config['breadcrumb'],
+            $entityPathConfig,
+            $data
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+
+            $em->persist($data);
+            $em->flush();
+
+            $redirectUrl = $this->getActionUrl($entityPathConfig, 'index', $data);
+            if ($form->has('saveAndStay') && $form->get('saveAndStay')->isClicked()) {
+                $redirectUrl = $this->getActionUrl($entityPathConfig, 'update', $data);
+            }
+
+            if ($request->isXmlHttpRequest()) {
+                return new JsonResponse(
+                    array(
+                        'success'  => true,
+                        'redirect' => $redirectUrl,
+                    )
+                );
+            }
+
+            return $this->redirect($redirectUrl);
+        } else {
+            $form->setData($data);
+        }
+
+        $form = $form->createView();
+        $renderVars['form'] = $form;
+        $renderVars['formFields'] = $formFields;
+
+        if (!empty($config['central']['viewLink']['action'])) {
+            $config['central']['viewLink']['url'] = $this->getActionUrl(
+                $entityPathConfig,
+                $config['central']['viewLink']['action'],
+                $data
+            );
+        }
 
         foreach ($config['central']['tabs'] as $tabSlug => $tabProperties) {
-            $config['central']['tabs'][$tabSlug]['name'] = $this->backendTranslator->trans(
-                $config['central']['tabs'][$tabSlug]['name']
-            );
-            if (!empty($tabProperties['formZones'])) {
-                foreach ($tabProperties['formZones'] as $formZoneSlug => $formZone) {
-                    if (isset($formZone['title'])) {
-                        $config['central']['tabs'][$tabSlug]['formZones'][$formZoneSlug]['title'] = $this->backendTranslator->trans(
-                            $config['central']['tabs'][$tabSlug]['formZones'][$formZoneSlug]['title']
-                        );
-                    }
-                }
+            if (isset($tabProperties['iframeContent'])) {
+                $config['central']['tabs'][$tabSlug]['iframeContent']['url'] = $this->getActionUrl(
+                    $entityPathConfig,
+                    $tabProperties['iframeContent']['action'],
+                    $data
+                );
             }
         }
 
-		$renderVars['central'] = $config['central'];
+        $renderVars['central'] = $config['central'];
 
-		foreach ($config['column']['panelZones'] as $key => $panelZone) {
-            $panelZone['headerLabel'] = $this->backendTranslator->trans($panelZone['headerLabel']);
+        foreach ($config['column']['panelZones'] as $key => $panelZone) {
+            $panelZone['form'] = $form;
+            $panelZone['formFields'] = $this->getFormFields($panelZone['fields'], $entityPathConfig);
 
-			$panelZone['form'] = $form;
-			$panelZone['formFields'] = $this->getFormFields($panelZone['fields'], $entityPathConfig);
+            unset($panelZone['fields']);
 
-			unset($panelZone['fields']);
+            if (isset($panelZone['footerListFormButtons'])) {
+                foreach ($panelZone['footerListFormButtons'] as $field => $footerListFormButton) {
+                    $footerListFormButton = array_merge($footerListFormButton, $config['formFields'][$field]);
+                    $footerListFormButton['form'] = $form;
 
-			if (isset($panelZone['footerListFormButtons'])) {
+                    $panelZone['footerListFormButtons'][$field] = $footerListFormButton;
+                }
+            }
+            $config['column']['panelZones'][$key] = $panelZone;
+        }
 
-				foreach ($panelZone['footerListFormButtons'] as $field => $footerListFormButton) {
+        $renderVars['column'] = $config['column'];
 
-					$footerListFormButton = array_merge($footerListFormButton, $config['formFields'][$field]);
-					$footerListFormButton['form'] = $form;
-                    $footerListFormButton['label'] = $this->backendTranslator->trans($footerListFormButton['label']);
+        $view = '@WHBackendTemplate/BackendTemplate/View/update.html.twig';
 
-					$panelZone['footerListFormButtons'][$field] = $footerListFormButton;
-				}
-			}
-			$config['column']['panelZones'][$key] = $panelZone;
-		}
+        $renderVars = $this->get('bk.wh.back.update_controller')->translateRenderVars($entityPathConfig, $renderVars);
 
-		$renderVars['column'] = $config['column'];
+        return $this->container->get('templating')->renderResponse(
+            $view,
+            $renderVars
+        );
+    }
 
-		return $this->container->get('templating')->renderResponse(
-			'@WHBackendTemplate/BackendTemplate/View/update.html.twig',
-			$renderVars
-		);
-	}
+    /**
+     * @Route("/delete/{id}", name="bk_wh_cms_page_delete")
+     *
+     * @param         $id
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction($id)
+    {
+        $deleteController = $this->get('bk.wh.back.delete_controller');
 
-	/**
-	 * @Route("/delete/{id}", name="bk_wh_cms_page_delete")
-	 *
-	 * @param         $id
-	 *
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function deleteAction($id)
-	{
-		$deleteController = $this->get('bk.wh.back.delete_controller');
+        return $deleteController->delete($this->getEntityPathConfig(), $id);
+    }
 
-		return $deleteController->delete($this->getEntityPathConfig(), $id);
-	}
+    /**
+     * @Route("/order/", name="bk_wh_cms_page_order")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function orderAction(Request $request)
+    {
+        $orderController = $this->get('bk.wh.back.order_controller');
 
-	/**
-	 * @Route("/order/", name="bk_wh_cms_page_order")
-	 *
-	 * @param Request $request
-	 *
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
-	public function orderAction(Request $request)
-	{
-		$orderController = $this->get('bk.wh.back.order_controller');
+        return $orderController->order($this->getEntityPathConfig(), $request);
+    }
 
-		return $orderController->order($this->getEntityPathConfig(), $request);
-	}
+    /**
+     * @param $action
+     *
+     * @return mixed
+     */
+    private function getOverrideConfig($action)
+    {
+        $ymlPath = $this->get('kernel')->getRootDir();
+        $ymlPath .= '/Resources/WHCmsBundle/config/Backend/Page/' . $action . '.yml';
 
-	/**
-	 * @param $action
-	 *
-	 * @return mixed
-	 */
-	private function getOverrideConfig($action)
-	{
-		$ymlPath = $this->get('kernel')->getRootDir();
-		$ymlPath .= '/Resources/WHCmsBundle/config/Backend/Page/' . $action . '.yml';
+        if (!file_exists($ymlPath)) {
+            throw new NotFoundHttpException(
+                'Le fichier de configuration n\'existe pas. Il devrait être ici : ' . $ymlPath
+            );
+        }
 
-		if (!file_exists($ymlPath)) {
-			throw new NotFoundHttpException(
-				'Le fichier de configuration n\'existe pas. Il devrait être ici : ' . $ymlPath
-			);
-		}
+        $config = Yaml::parse(file_get_contents($ymlPath));
+        if ($this->validConfig($config)) {
+            return $config;
+        }
 
-		$config = Yaml::parse(file_get_contents($ymlPath));
-		if ($this->validConfig($config)) {
-			return $config;
-		}
-
-		return array();
-	}
+        return array();
+    }
 
 }
